a7ade23c461744574d2724e85439d72e4e3bda61
jy-work <jychoe.work@gmail.com>
2021-12-30 07:13:53 +0900

    uio 페이지 수정

--- README.md
+++ README.md
@@ -5,8 +5,9 @@
 > - [nunjucks 템플릿](https://mozilla.github.io/nunjucks/templating.html)을 사용하여 HTML 작성합니다.
 > - 스타일은 scss 코드로 작성합니다.
 
-1. npm install (node 버전 14.15.3)
-2. `gulp` 명령어 입력
+1. `$ npm install` (node 버전 12이상)
+2. `$ npm install gulp -g`
+3. `gulp` 명령어 입력
 
 ### 폴더 구조
 - `src > html > templates` 폴더에 html 작성 > `src > html > dist` 폴더에 출력
@@ -19,10 +20,20 @@
     │  ├─dist
     │  │
     │  └─templates
-    │      └─include
+    │  
     ├─img
     └─style
         ├─css
         ├─font
         └─scss
 ```
+
+### UI 확인 방법
+- `src > html > dist > uio.html` 페이지 확인
+
+### 이미지 사용방법
+구글 Material UI 아이콘을 사용하였습니다.
+1. https://fonts.google.com/icons 에서 아이콘 검색
+2. 아이콘 선택 후 오른쪽 code 복사
+  > ex) `<span class="material-icons-outlined">home</span>`
+3. 색을 변경할 경우, CSS `fill: #컬러값;` 으로 변경
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,19 +1,29 @@
-// $(function () {
-//     var selectTarget = $('.selectbox select');
+$(document).ready(function(){
 
-//     // focus 가 되었을 때와 focus 를 잃었을 때
-//     selectTarget.on({
-//         'focus': function () {
-//             $(this).parent().addClass('focus');
-//         },
-//         'blur': function () {
-//             $(this).parent().removeClass('focus');
-//         }
-//     });
+    // 테이블 선택 시, on 클래스 추가하여 선택 표시
+    const tableRow = $("table tr");
 
-//     selectTarget.change(function () {
-//         var select_name = $(this).children('option:selected').text();
-//         $(this).siblings('label').text(select_name);
-//         $(this).parent().removeClass('focus');
-//     });
-// });
+    tableRow.click(function(){
+        $(this).addClass("on");
+        tableRow.not($(this)).removeClass("on");
+    })
+
+    // 페이지네이션 선택 시, on 클래스 추가하여 선택 표시
+    const pageLink = $(".pagenation .pages .link");
+
+    pageLink.click(function(){
+        $(this).addClass("on");
+        pageLink.not($(this)).removeClass("on");
+    })
+
+    // 파일 추가 시, 파일 경로 출력
+    var fileName = $("#file").val();
+    $(".wrap_uio #file").on("change",function(){
+        if(window.FileReader){
+            var filename = $(this)[0].files[0].name;
+        } else {
+            var filename = $(this).val().split("/").pop().split("\\").pop();
+        }
+        $(this).siblings(".file_name").text(filename).css("color", "#333");
+    });
+ });
